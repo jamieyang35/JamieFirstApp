9080870cea511f674205d28cecfc7b2e86f605a5
stoller <stoller@stollerde-macbook-pro.local>
2019-03-05 14:36:42 +0800

    Initial Commit

---
+++ JamieFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JamieFirstApp
+//
+//  Created by stoller on 2019/3/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
